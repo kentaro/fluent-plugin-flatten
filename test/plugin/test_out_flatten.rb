41b20c980ac79be2ae1b7be775056aa9d41c9b14
Kenji Okimoto <okimoto@clear-code.com>
2016-11-11 14:09:47 +0900

    Remove redundant numerical suffix

--- test/plugin/test_out_flatten.rb
+++ test/plugin/test_out_flatten.rb
@@ -17,30 +17,30 @@ class FlattenOutputTest < Test::Unit::TestCase
 
   sub_test_case "configure" do
     test "when `inner_key` option is not set" do
-      d1 = create_driver
+      d = create_driver
 
-      assert_equal 'foo',                d1.instance.key
-      assert_equal 'flattened.',         d1.instance.add_tag_prefix
-      assert_equal /^test\./,            d1.instance.remove_tag_prefix
-      assert_equal 'value',              d1.instance.inner_key          # default value
+      assert_equal 'foo',                d.instance.key
+      assert_equal 'flattened.',         d.instance.add_tag_prefix
+      assert_equal /^test\./,            d.instance.remove_tag_prefix
+      assert_equal 'value',              d.instance.inner_key          # default value
     end
 
     test "when `inner_key` is set" do
-      d2 = create_driver(%[
+      d = create_driver(%[
         key               foo
         add_tag_prefix    flattened.
         remove_tag_prefix test.
         inner_key         value_for_flat_key
       ])
 
-      assert_equal 'foo',                d2.instance.key
-      assert_equal 'flattened.',         d2.instance.add_tag_prefix
-      assert_equal /^test\./,            d2.instance.remove_tag_prefix
-      assert_equal 'value_for_flat_key', d2.instance.inner_key
+      assert_equal 'foo',                d.instance.key
+      assert_equal 'flattened.',         d.instance.add_tag_prefix
+      assert_equal /^test\./,            d.instance.remove_tag_prefix
+      assert_equal 'value_for_flat_key', d.instance.inner_key
     end
 
     test "when `parse_json` is false" do
-      d3 = create_driver(%[
+      d = create_driver(%[
         key               foo
         add_tag_prefix    flattened.
         remove_tag_prefix test.
@@ -48,11 +48,11 @@ class FlattenOutputTest < Test::Unit::TestCase
         parse_json        false
       ])
 
-      assert_equal 'foo',                d3.instance.key
-      assert_equal 'flattened.',         d3.instance.add_tag_prefix
-      assert_equal /^test\./,            d3.instance.remove_tag_prefix
-      assert_equal 'value_for_flat_key', d3.instance.inner_key
-      assert_equal false,         d3.instance.parse_json
+      assert_equal 'foo',                d.instance.key
+      assert_equal 'flattened.',         d.instance.add_tag_prefix
+      assert_equal /^test\./,            d.instance.remove_tag_prefix
+      assert_equal 'value_for_flat_key', d.instance.inner_key
+      assert_equal false,         d.instance.parse_json
     end
 
     test "mandatory parameters are missing" do
@@ -98,31 +98,31 @@ class FlattenOutputTest < Test::Unit::TestCase
   sub_test_case "emit" do
     test "default config" do
       # test1 default config
-      d1 = create_driver
+      d = create_driver
 
-      d1.run(default_tag: "test") do
-        d1.feed( 'foo' => '{"bar" : "baz"}', 'hoge' => 'fuga' )
-        d1.feed( 'foo' => '{"bar" : {"qux" : "quux", "hoe" : "poe" }, "baz" : "bazz" }', 'hoge' => 'fuga' )
+      d.run(default_tag: "test") do
+        d.feed( 'foo' => '{"bar" : "baz"}', 'hoge' => 'fuga' )
+        d.feed( 'foo' => '{"bar" : {"qux" : "quux", "hoe" : "poe" }, "baz" : "bazz" }', 'hoge' => 'fuga' )
       end
-      events1 = d1.events
+      events = d.events
 
-      assert_equal 4, events1.count
+      assert_equal 4, events.count
 
       # ["flattened.foo.bar", 1354689632, {"value"=>"baz"}]
-      assert_equal     'flattened.foo.bar', events1[0][0]
-      assert_equal                   'baz', events1[0][2]['value']
+      assert_equal     'flattened.foo.bar', events[0][0]
+      assert_equal                   'baz', events[0][2]['value']
 
       # ["flattened.foo.bar.qux", 1354689632, {"value"=>"quux"}]
-      assert_equal 'flattened.foo.bar.qux', events1[1][0]
-      assert_equal                  'quux', events1[1][2]['value']
+      assert_equal 'flattened.foo.bar.qux', events[1][0]
+      assert_equal                  'quux', events[1][2]['value']
 
       # ["flattened.foo.bar.hoe", 1354689632, {"value"=>"poe"}]
-      assert_equal 'flattened.foo.bar.hoe', events1[2][0]
-      assert_equal                   'poe', events1[2][2]['value']
+      assert_equal 'flattened.foo.bar.hoe', events[2][0]
+      assert_equal                   'poe', events[2][2]['value']
 
       # ["flattened.foo.bar.baz", 1354689632, {"value"=>"bazz"}]
-      assert_equal     'flattened.foo.baz', events1[3][0]
-      assert_equal                  'bazz', events1[3][2]['value']
+      assert_equal     'flattened.foo.baz', events[3][0]
+      assert_equal                  'bazz', events[3][2]['value']
     end
 
     test "parse_json is set false" do
@@ -139,33 +139,33 @@ class FlattenOutputTest < Test::Unit::TestCase
         d.feed( 'foo' => {'bar' => {'qux' => 'quux', 'hoe' => 'poe' }, 'baz' => 'bazz' }, 'hoge' => 'fuga' )
         d.feed( 'foo' => {'bar hoge' => 'baz', 'hoe baz' => 'poe'}, 'hoge' => 'fuga' )
       end
-      events2 = d.events
+      events = d.events
 
-      assert_equal 6, events2.count
+      assert_equal 6, events.count
 
       # ["flattened.foo.bar", 1354689632, {"value"=>"baz"}]
-      assert_equal          'flattened.foo.bar', events2[0][0]
-      assert_equal                        'baz', events2[0][2]['value']
+      assert_equal          'flattened.foo.bar', events[0][0]
+      assert_equal                        'baz', events[0][2]['value']
 
       # ["flattened.foo.bar.qux_qux", 1354689632, {"value"=>"quux"}]
-      assert_equal      'flattened.foo.bar.qux', events2[1][0]
-      assert_equal                       'quux', events2[1][2]['value']
+      assert_equal      'flattened.foo.bar.qux', events[1][0]
+      assert_equal                       'quux', events[1][2]['value']
 
       # ["flattened.foo.bar.hoe", 1354689632, {"value"=>"poe"}]
-      assert_equal      'flattened.foo.bar.hoe', events2[2][0]
-      assert_equal                        'poe', events2[2][2]['value']
+      assert_equal      'flattened.foo.bar.hoe', events[2][0]
+      assert_equal                        'poe', events[2][2]['value']
 
       # ["flattened.foo.bar.baz", 1354689632, {"value"=>"bazz"}]
-      assert_equal          'flattened.foo.baz', events2[3][0]
-      assert_equal                       'bazz', events2[3][2]['value']
+      assert_equal          'flattened.foo.baz', events[3][0]
+      assert_equal                       'bazz', events[3][2]['value']
 
       # ["flattened.foo.bar_hoge", 1354689632, {"value"=>"baz"}]
-      assert_equal     'flattened.foo.bar_hoge', events2[4][0]
-      assert_equal                        'baz', events2[4][2]['value']
+      assert_equal     'flattened.foo.bar_hoge', events[4][0]
+      assert_equal                        'baz', events[4][2]['value']
 
       # ["flattened.foo.hoe_baz", 1354689632, {"value"=>"baz"}]
-      assert_equal      'flattened.foo.hoe_baz', events2[5][0]
-      assert_equal                        'poe', events2[5][2]['value']
+      assert_equal      'flattened.foo.hoe_baz', events[5][0]
+      assert_equal                        'poe', events[5][2]['value']
     end
   end
 end
